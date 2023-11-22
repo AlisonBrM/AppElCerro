3d04d1b47977281cc284d5c2f7f23ff5836070d1
JorgeForero <>
2023-11-21 20:57:10 -0500

    Inicio proyecto

---
+++ src/main/java/com/mycompany/modelo/entity/Producto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.modelo.entity;
+
+/**
+ *
+ * @author forer
+ */
+public class Producto {
+    String id;
+    String nombre;
+    String tipo;
+    float precio;
+    String descripcion;
+    int cantidad;
+    
+}
---
+++ src/main/java/com/mycompany/modelo/entity/Usuario.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.modelo.entity;
+
+/**
+ *
+ * @author forer
+ */
+public class Usuario {
+    private String cedula;
+    private String nombre;
+    private String direccion;
+    private String correo;
+    //FormasDePago pago;
+
+    public Usuario(String cedula, String nombre, String direccion, String correo) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.correo = correo;
+    }
+
+    public Usuario(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public Usuario() {
+    }
+    
+
+    /**
+     * @return the cedula
+     */
+    public String getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+}
