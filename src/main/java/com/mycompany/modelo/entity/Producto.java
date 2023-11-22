b2abba98aff7c44455b10a424dea601274ed9543
JorgeForero <forer@DESKTOP-UFTL541>
2023-11-21 21:11:32 -0500

    Inicio proyecto

--- src/main/java/com/mycompany/modelo/entity/Producto.java
+++ src/main/java/com/mycompany/modelo/entity/Producto.java
@@ -9,11 +9,113 @@ package com.mycompany.modelo.entity;
  * @author forer
  */
 public class Producto {
-    String id;
-    String nombre;
-    String tipo;
-    float precio;
-    String descripcion;
-    int cantidad;
+    
+    private String id;
+    private String nombre;
+    private String tipo;
+    private float precio;
+    private String descripcion;
+    private int cantidad;
+
+    public Producto(String id, String nombre, String tipo, float precio, String descripcion, int cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.cantidad = cantidad;
+    }
+
+    public Producto(String id) {
+        this.id = id;
+    }
+
+    public Producto() {
+    }
+    
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the precio
+     */
+    public float getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the cantidad
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+   
     
 }
