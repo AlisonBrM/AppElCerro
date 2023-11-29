5d495760f1556dc40a4f1e940d712b3c43dd77d4
Alison Martinez <Alison Martinez@DESKTOP-PARSK9P>
2023-11-28 19:08:08 -0500

    Se terminó la clase PromocionesDao y CarritoDao

---
+++ src/main/java/com/mycompany/modelo/dao/TestPromocionesDao.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.modelo.dao;
+
+import com.mycompany.modelo.entity.Producto;
+import com.mycompany.modelo.entity.Promociones;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+/**
+ *
+ * @author Alison Martinez
+ */
+public class TestPromocionesDao {
+    public static void main(String[] args) throws ParseException {
+        PromocionesDao promo = new PromocionesDao();
+        Producto producto = new Producto("1");
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+        Date fecha_inicio = formato.parse("2023-12-1");
+        Date fecha_fin = formato.parse("2023-12-27");
+        promo.crear(new Promociones("1112233",producto , "25% descuento mes de Diciembre", 25 , fecha_inicio, fecha_fin));
+    }
+}
